0dac49f26dec88628629046e2e57c4ea481a9af0
DamianoCasolari <damianocasolari@gmail.com>
2023-03-20 18:19:51 +0100

    add multibomb in the grid in the end of game

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,7 @@ buttonEl.addEventListener("click", function () {
 
 })
 
-//Declair Function to add addEventListener to cell of grid
+//Declair Function to add addEventListener to single cell of grid
 function changeBackground(arrayElement, arrayBomb, cellsGood) {
     for (let i = 0; i < arrayElement.length; i++) {
         const singleCell = arrayElement[i]
@@ -114,7 +114,7 @@ function changeBackground(arrayElement, arrayBomb, cellsGood) {
                 arrayCellGood.push(this.innerText);
                 cellsGood++;
                 if (cellsGood == arrayElement.length - arrayBomb.length) {
-                    const imageWin = `  <div class="bomb_you_lose">
+                    const imageWin = `<div class="bomb_you_lose">
                 <img src="./assets/img/istockphoto-587803682-612x612.jpg" alt="artificere_you_win">
                 <div class="text-white text-center">YOU WIN<br> hai evitato una bomba ${cellsGood} volte</div>
                 </div>`
@@ -123,11 +123,17 @@ function changeBackground(arrayElement, arrayBomb, cellsGood) {
 
             } else if (arrayCellGood.includes(this.innerText) && !arrayBomb.includes(Number(this.innerText))) { }
             else {
+                for( let i = 0; i < arrayElement.length; i++ ){
+                    let CellofBomb = arrayElement[i];
+                    if (arrayBomb.includes(Number(CellofBomb.innerText))){
+                    CellofBomb.innerHTML = `<img class="w-100" src="./assets/img/sasdad_preview_rev_1.png" alt=" bomba, you lose">`}
+                }
                 const imageBomb = `  <div class="bomb_you_lose">
                 <img src="./assets/img/sasdad_preview_rev_1.png" alt=" bomba, you lose">
                 <div class="text-white text-center">YOU LOSE <br> hai evitato una bomba ${cellsGood} volte</div>
                 </div>`
                 document.querySelector(".main_container").insertAdjacentHTML("beforeend", imageBomb)
+
 
             }
         })
